bcb1cbb8d583d6ff8c082e4cbc6d1f8a9ea10900
roman.vorotnyak <Roman.Vorotnjak_ext@leroymerlin.ru>
2022-03-30 23:06:21 +0300

    fix

--- src/test/java/tests/AutomationPracticeFormWithPageObjectTests.java
+++ src/test/java/tests/AutomationPracticeFormWithPageObjectTests.java
@@ -6,6 +6,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.PracticeFormPage;
 
 import java.io.File;
@@ -24,6 +25,12 @@ public class AutomationPracticeFormWithPageObjectTests {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
 
     @Test
