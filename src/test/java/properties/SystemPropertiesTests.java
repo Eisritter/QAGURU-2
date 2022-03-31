bfadcc773b8d82d77ed8f5ec83c1c7ba34b77205
roman.vorotnyak <Roman.Vorotnjak_ext@leroymerlin.ru>
2022-03-31 22:20:46 +0300

    add homework 11

---
+++ src/test/java/properties/SystemPropertiesTests.java
@@ -0,0 +1,23 @@
+package properties;
+
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+        @Test
+        void someTest1() {
+            String browser = System.getProperty("browser");
+            System.out.println(browser); // null
+
+        }
+        @Test
+        void someTest2() {
+            System.setProperty("some_browser", "safari");
+            String browser = System.getProperty("browser");
+            System.out.println(browser); //safari
+        }
+        @Test
+        void someTest3() {
+            String browser = System.getProperty("browser", "opera");
+            System.out.println(browser); // opera
+        }
+    }
