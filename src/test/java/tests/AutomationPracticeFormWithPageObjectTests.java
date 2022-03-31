c1e94fd3c6f424a7287b1b6163f770874d66cae1
roman.vorotnyak <Roman.Vorotnjak_ext@leroymerlin.ru>
2022-03-31 22:47:07 +0300

    add homework 11

--- src/test/java/tests/AutomationPracticeFormWithPageObjectTests.java
+++ src/test/java/tests/AutomationPracticeFormWithPageObjectTests.java
@@ -24,6 +24,7 @@ public class AutomationPracticeFormWithPageObjectTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
