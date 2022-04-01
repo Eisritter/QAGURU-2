34ece2ac1092671c28f8644f4a501b3d452fc525
roman.vorotnyak <Roman.Vorotnjak_ext@leroymerlin.ru>
2022-04-01 23:28:26 +0300

    add homework 12

--- src/test/java/tests/SelenideSoftAssertions.java
+++ src/test/java/tests/SelenideSoftAssertions.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -31,6 +32,7 @@ public class SelenideSoftAssertions {
     }
 
     @Test
+    @Disabled
     void WikiSoftAssertions() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
